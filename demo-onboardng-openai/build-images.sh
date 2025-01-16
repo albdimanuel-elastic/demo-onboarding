ad1a855c67f6edc9215e1c22f956334bca84c82d
albdimanuel-elastic <alberto.dimanuel@elastic.co>
2025-01-16 16:11:57 +0100

    Delete demo-onboardng-openai directory

--- demo-onboardng-openai/build-images.sh
+++
@@ -1,8 +0,0 @@
-# Build and push the frontend
-docker build --no-cache -t albertodimanuel275/frontend-demo-onboarding:latest -f ./src/frontend/Dockerfile ./src/frontend
-docker push albertodimanuel275/frontend-demo-onboarding:latest
-
-# Build and push the backend
-
-docker build --no-cache -t albertodimanuel275/backend-demo-onboarding:latest -f ./src/backend/Dockerfile ./src/backend
-docker push albertodimanuel275/backend-demo-onboarding:latest
